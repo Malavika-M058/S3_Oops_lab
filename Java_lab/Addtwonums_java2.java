3b28a0bead53f582d19ab36f43cbcceeb15e01e2
Malavika-M058 <malavikamanoj058@gmail.com>
2020-10-29 13:23:43 +0530

    another file

---
+++ Java_lab/Addtwonums_java2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class addtwonums
+ {
+   public static void main(String args[])
+   { 
+     int a=3,b=5,sum=0;
+     sum=a+b;
+     System.out.println(sum);
+   }
+ }
