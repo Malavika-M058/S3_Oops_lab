5002ec52b695a92d31056b9a8385d70593b67b5f
Malavika-M058 <malavikamanoj058@gmail.com>
2020-10-29 13:22:31 +0530

    another file

---
+++ Java_lab/Armstrong_java10.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ 
+public class Armstrong {
+
+    public static void main(String[] args) {
+
+        int num = 371, originalNumber, rem, result = 0;
+
+        originalNumber = num;
+
+        while (originalNumber != 0)
+        {
+            rem = originalNumber % 10;
+            result += Math.pow(rem , 3);
+            originalNumber /= 10;
+        }
+
+        if(result == num)
+            System.out.println(num+ " is an Armstrong number.");
+        else
+            System.out.println(num + " is not an Armstrong number.");
+    }
+}
