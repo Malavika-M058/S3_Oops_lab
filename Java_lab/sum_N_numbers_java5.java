3c07bb3dbf64e064f8e04906c47e891517f6822a
Malavika-M058 <malavikamanoj058@gmail.com>
2020-10-29 13:15:33 +0530

    upload files

---
+++ Java_lab/sum_N_numbers_java5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class sum_N_numbers
+ {
+   public static void main(String args[])
+   { 
+    int num=5,sum=0;
+    for(int i=1;i<=5;i++)
+    {
+      sum+=i;
+    }
+    System.out.println(sum);
+   }
+ }
