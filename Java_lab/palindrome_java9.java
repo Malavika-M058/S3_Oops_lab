e3cbab0f9ee4e20c9e6392559aa7c35e30621b6e
Malavika-M058 <malavikamanoj058@gmail.com>
2020-10-29 13:21:50 +0530

    another file

---
+++ Java_lab/palindrome_java9.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ public class Palindrome {
+
+    public static void main(String[] args) {
+
+        int num = 121, rev = 0, remainder, original;
+
+        original = num;
+
+         
+        while( num != 0 )
+        {
+            remainder = num % 10;
+            rev = rev * 10 + remainder;
+            num  /= 10;
+        }
+
+        
+        if (original == rev)
+            System.out.println(original + " is a palindrome.");
+        else
+            System.out.println(original + " is not a palindrome.");
+    }
+}
